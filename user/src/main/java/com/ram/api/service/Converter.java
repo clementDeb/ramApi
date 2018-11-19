54a74e2fe6be20edc64b776694c5d6ec450693fe
clement <clem.debrosse@gmail.com>
2018-11-19 22:23:39 +0100

    CDE update converter and test class

--- user/src/main/java/com/ram/api/service/Converter.java
+++ user/src/main/java/com/ram/api/service/Converter.java
@@ -3,24 +3,25 @@ package com.ram.api.service;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
-import com.ram.api.persistance.UserEntity;
-import com.ram.api.user.model.User;
+import com.ram.api.model.superclass.Person;
+import com.ram.api.persistance.PersonEntity;
 
-@Mapper(componentModel="spring")
+@Mapper
 public interface Converter {
 	
-	Converter INSTANCE = Mappers.getMapper( Converter.class );
+	//Not used if CDI is done with Spring
+	Converter INSTANCE = Mappers.getMapper(Converter.class);
 	
 	/**Method to convert an entity into a dto
 	 * @param user
 	 * @return the converted entity into dto
 	 */
-	User userEntityToUser(UserEntity user);
+	Person entityToDto(PersonEntity entity);
 	
 	/**Method to convert a dto into an entity
 	 * @param user
 	 * @returnthe converted dto into entity
 	 */
-	UserEntity userToUserEntity(User user);
+	PersonEntity dtoToEntity(Person dto);
 
 }
--- user/src/test/java/com/ram/api/user/ConverterTest.java
+++ user/src/test/java/com/ram/api/user/ConverterTest.java
@@ -3,23 +3,24 @@ package com.ram.api.user;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import com.ram.api.model.User;
+import com.ram.api.model.superclass.Person;
+import com.ram.api.persistance.PersonEntity;
 import com.ram.api.persistance.UserEntity;
 import com.ram.api.service.Converter;
-import com.ram.api.user.model.User;
 
 public class ConverterTest {
-
+	
 	@Test
 	public void userToUserEntityTest() {
-		User user = new User();
-		user.setFirstName("firstname");
-		user.setNickName("nickName");
+		Person user = new User("test", "test");
 		
-		UserEntity entity = Converter.INSTANCE.userToUserEntity(user);
+		PersonEntity entity = Converter.INSTANCE.dtoToEntity(user);
 		
 		assertEquals(entity.getFirstName(), user.getFirstName());
-		assertEquals(entity.getNickName(), user.getNickName());
+		assertEquals(entity.getLastName(), user.getLastName());
 		
 	}
 	
