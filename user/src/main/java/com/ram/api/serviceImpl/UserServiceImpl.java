b60ca5e92cfef269d398ba65cef6cf8c72288b91
clement <clem.debrosse@gmail.com>
2018-11-19 22:21:52 +0100

    CDE update service

--- user/src/main/java/com/ram/api/serviceImpl/UserServiceImpl.java
+++ user/src/main/java/com/ram/api/serviceImpl/UserServiceImpl.java
@@ -4,19 +4,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ram.api.persistance.UserEntity;
-import com.ram.api.repositories.UserRepository;
+import com.ram.api.repositories.superclass.PersonRepository;
 import com.ram.api.service.UserService;
 
+import lombok.extern.log4j.Log4j2;
+
 @Service
+@Log4j2
 public class UserServiceImpl implements UserService{
 	
+	//private final Logger log = LogManager.getLogger(UserServiceImpl.class);
+	
 	@Autowired
-	UserRepository repository;
+	PersonRepository<UserEntity> userRepository;
 
 	@Override
 	public UserEntity createAccount(UserEntity user) {
+		log.debug("in userServiceImpl");
 		UserEntity userSaved = new UserEntity();
-		userSaved = repository.save(user);
+		userSaved = userRepository.save(user);
 		return userSaved;
 	}
 
