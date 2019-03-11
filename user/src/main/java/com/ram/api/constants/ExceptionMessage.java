e7f78d72423ea5637a05643f6828ace5a77ca35c
CDE <cdebrosse@sqli.com>
2019-03-11 16:41:23 +0100

    CDE add external property file for exception messages

---
+++ user/src/main/java/com/ram/api/constants/ExceptionMessage.java
@@ -0,0 +1,20 @@
+package com.ram.api.constants;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("/home/ramApi/exceptionMessage.properties")
+public class ExceptionMessage {
+	
+	@Value("${exception.USER_NOT_FOUND_MSG}")
+	public static String USER_NOT_FOUND_MSG;
+	
+	@Value("${exception.EMAIL_ALREADY_EXISTS}")
+	public static String EMAIL_ALREADY_EXISTS;
+	
+	@Value("${exception.ADRESS_NOT_FOUND}")
+	public static String ADRESS_NOT_FOUND;
+
+}
