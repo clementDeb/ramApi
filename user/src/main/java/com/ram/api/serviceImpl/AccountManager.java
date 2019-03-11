0137ed3ce9632bac137a42a6a8028de61692348e
CDE <cdebrosse@sqli.com>
2019-03-11 12:07:03 +0100

    CDE add class to manage the account creation

---
+++ user/src/main/java/com/ram/api/serviceImpl/AccountManager.java
@@ -0,0 +1,32 @@
+package com.ram.api.serviceImpl;
+
+import java.util.ArrayList;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import antlr.collections.List;
+
+public class AccountManager implements PasswordEncoder{
+	
+	private final BCryptPasswordEncoder ENCODER;
+	
+	public AccountManager() {
+		ENCODER = new BCryptPasswordEncoder();
+	}
+
+	@Override
+	public String encode(CharSequence rawPassword) {
+		return ENCODER.encode(rawPassword);
+	}
+
+	@Override
+	public boolean matches(CharSequence rawPassword, String encodedPassword) {
+		return ENCODER.matches(rawPassword, encodedPassword);
+	}
+	
+	public boolean loginExist (String login, List<String> logins) {
+		return logins.stream().anyMatch(l -> l.equals(login));
+	}
+
+}
