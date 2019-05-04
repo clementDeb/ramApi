dd4e75057111e9b60fa4fd70f17e4e9aaf1b34a9
clement <clem.debrosse@gmail.com>
2019-05-04 22:47:44 +0200

    CDE add cache config

--- user/src/main/java/com/ram/api/serviceImpl/AdressServiceImpl.java
+++ user/src/main/java/com/ram/api/serviceImpl/AdressServiceImpl.java
@@ -3,6 +3,7 @@ package com.ram.api.serviceImpl;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
@@ -17,6 +18,7 @@ import lombok.extern.log4j.Log4j2;
 
 @Service
 @Log4j2
+@CacheConfig(cacheManager="adressCacheManager")
 public class AdressServiceImpl implements AdressService{
 	
 	private static final String ADRESS_NOT_FOUND_MSG = "adress not found";
@@ -32,21 +34,21 @@ public class AdressServiceImpl implements AdressService{
 	}
 
 	@Override
-	@Cacheable
+	@Cacheable(value="adress")
 	public AdressEntity retrieveAdress(long adressId) throws AdressNotFoundException {
 		Optional<AdressEntity> adressOptional = adressRepository.findById(adressId);
 		return adressOptional.orElseThrow(() -> new AdressNotFoundException(ADRESS_NOT_FOUND_MSG));
 	}
 
 	@Override
-	@CachePut
+	@CachePut(value="adress")
 	public AdressEntity updateAdress(AdressEntity adress) {
 		log.debug("in updateAdress");
 		return adressRepository.save(adress);
 	}
 
 	@Override
-	@CacheEvict
+	@CacheEvict(value="adress")
 	public void deleteAdress(AdressEntity entity) {
 		adressRepository.delete(entity);
 	}
