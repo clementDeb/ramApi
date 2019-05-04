b0f861f940eed92a35b8eb10c89757d3953e647f
clement <clem.debrosse@gmail.com>
2019-05-04 22:35:09 +0200

    CDE update app conf to disable api call default security

--- user/src/main/java/com/ram/api/RamApi.java
+++ user/src/main/java/com/ram/api/RamApi.java
@@ -1,19 +1,17 @@
 package com.ram.api;
 
-import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
-import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 @EntityScan("com.ram.api.persistance")
 @EnableJpaRepositories("com.ram.api.repositories")
 @ComponentScan("com.ram.api")
