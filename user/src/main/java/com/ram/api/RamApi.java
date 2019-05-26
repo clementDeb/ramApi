ec15a9bfcb42b2f4626c683500bdbba5d818535c
clement <clem.debrosse@gmail.com>
2019-05-26 22:42:10 +0200

    CDE refacto bean factory for cache manager

--- user/src/main/java/com/ram/api/RamApi.java
+++ user/src/main/java/com/ram/api/RamApi.java
@@ -1,12 +1,16 @@
 package com.ram.api;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -25,13 +29,11 @@ public class RamApi
     
     @Bean
     public CacheManager userCacheManager() {
-    	ConcurrentMapCacheManager cacheManager = new ConcurrentMapCacheManager("user");
-        return cacheManager;
-    }
-    
-    @Bean
-    public CacheManager adressCacheManager() {
-    	ConcurrentMapCacheManager cacheManager = new ConcurrentMapCacheManager("adress");
+    	SimpleCacheManager cacheManager = new SimpleCacheManager();
+    	cacheManager.setCaches(Arrays.asList(
+    			new ConcurrentMapCache("user"),
+    			new ConcurrentMapCache("adress")
+    			));
         return cacheManager;
     }
     
